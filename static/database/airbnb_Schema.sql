4b2ceb14212d03ba07ea0b52ecd013130216714b
RaeAnnG <raeanng516@gmail.com>
2024-01-25 16:50:36 -0600

    Project 3 Airbnb PgAdmin Schema Update 1.25.24

--- static/database/airbnb_Schema.sql
+++ static/database/airbnb_Schema.sql
@@ -1,24 +1,25 @@
 CREATE TABLE "listings_KP_All_data_Clean" (
-    "id" int   NOT NULL,
+    "id" bigint   NOT NULL,
     "host_id" int   NOT NULL,
     "host_name" varchar (30)   NOT NULL,
 	"host_has_profile_pick" varchar NOT NULL,
 	"host_identity_verified" varchar NOT NULL,
 	"neighbourhood_cleansed" varchar NOT NULL,
 	"neighbourhood_group_cleansed" varchar NOT NULL,
-	"latitude" int NOT NULL,
-	"longitude" int NOT NULL,
+	"latitude" decimal NOT NULL,
+	"longitude" decimal NOT NULL,
 	"property_type" varchar NOT NULL,
 	"room_type" varchar NOT NULL,
 	"accommodates" int NOT NULL,
 	"bathrooms_text" varchar NOT NULL,
 	"beds" int NOT NULL,
-	"price" int NOT NULL,
+	"price" decimal NOT NULL,
 	"has_availability" varchar NOT NULL,
-	"review_scores_rating" int NOT NULL,
-	
-	
-    PRIMARY KEY (
+	"review_scores_rating" decimal NOT NULL,
+	"instant_bookable" varchar NOT NULL,
+	PRIMARY KEY (
         "id"
-     )
+	)
 );
+
+DROP Table "listings_KP_All_data_Clean"
